b363b43b74624c4014ad370881011a1d30caf0be
Vansh Charak <vanshcharak05@gmail.com>
2026-07-30 09:06:24 +0000

    Refactor Customer class for encapsulation
    Updated fields to private access and added getter methods.

--- src/main/java/com/finance/frauddetection/model/Customer.java
+++ src/main/java/com/finance/frauddetection/model/Customer.java
@@ -1,10 +1,11 @@
 package com.finance.frauddetection.model;
 
 public class Customer {
-    int id;
-    String name;
-    String accountNumber;
-    String registeredCountry;
+
+    private int id;
+    private String name;
+    private String accountNumber;
+    private String registeredCountry;
 
     public Customer(int id, String name, String accountNumber, String registeredCountry) {
         this.id = id;
@@ -13,7 +14,19 @@ public class Customer {
         this.registeredCountry = registeredCountry;
     }
 
-    void getDetails(){}
-    void setDetails(){}
+    public int getId() {
+        return id;
+    }
 
+    public String getRegisteredCountry() {
+        return registeredCountry;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
 }
